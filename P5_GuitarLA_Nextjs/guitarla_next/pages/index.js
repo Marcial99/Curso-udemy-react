8a2db0f57ad3ce7b9491bc726fbff15ff3db0864
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-08-05 18:41:32 -0500

    Avance en el proyecto 4, React router y formik para validacion de formularios

---
+++ P5_GuitarLA_Nextjs/guitarla_next/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/Link";
+import Layout from "../components/Layout";
+export default function Home() {
+  return (
+    <Layout pagina="Inicio">
+      <h1>Desde inicio</h1>
+    </Layout>
+  );
+}
---
+++ P5_GuitarLA_Nextjs/guitarla_next/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/Link";
+import Layout from "../components/Layout";
+const Nosotros = () => {
+  return (
+    <Layout pagina="Nosotros">
+      <h1>Dede nosotros</h1>
+    </Layout>
+  );
+};
+
+export default Nosotros;

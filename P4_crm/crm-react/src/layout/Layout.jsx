4ab3e8d46a899c4d965e40a394cd66291ff829f9
Marcial99 <17031116@itcelaya.edu.mx>
2022-08-08 01:12:19 -0500

    Reestructuración del proyecto para poder desplegar en netlify

--- P4_crm/crm-react/src/layout/Layout.jsx
+++ P4_crm/crm-react/src/layout/Layout.jsx
@@ -13,17 +13,17 @@ const Layout = () => {
         <nav className="mt-10">
           <Link
             className={`${
-              urlActual === "/clientes" ? "text-blue-300" : "text-white"
+              urlActual === "/" ? "text-blue-300" : "text-white"
             } text-2xl block mt-2 hover:text-blue-300 `}
-            to="/clientes"
+            to="/"
           >
             Clientes
           </Link>
           <Link
             className={`${
-              urlActual === "/clientes/nuevo" ? "text-blue-300" : "text-white"
+              urlActual === "/nuevo" ? "text-blue-300" : "text-white"
             } text-2xl block mt-2 hover:text-blue-300 `}
-            to="/clientes/nuevo"
+            to="/nuevo"
           >
             Nuevo cliente
           </Link>

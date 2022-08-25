60c8cb2527de2dc7c9a1232190c3499f768cf027
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-08-25 13:20:30 -0500

    Inicio del proyecto 8, clima app

---
+++ p8_clima_app/clima_app/src/components/Formulario.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import useClima from "../hooks/useClima";
+const Formulario = () => {
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda } = useClima();
+  const { pais, ciudad } = busqueda;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(busqueda).includes("")) {
+    }
+  };
+  return (
+    <div className="contenedor">
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input
+            type="text"
+            id="ciudad"
+            name="ciudad"
+            onChange={datosBusqueda}
+            value={ciudad}
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">Pais</label>
+          <select id="pais" name="pais" value={pais}>
+            <option value="">-- Seleccion un país --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+        <input type="submit" value="Consultar clima" />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
